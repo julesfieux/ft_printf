10fed783ae5f09bc5333698dde543b67fc1c61fd
julesfieux <julesfieux@yahoo.fr>
2021-03-17 12:22:05 +0100

    norm

--- ft_verification.c
+++ ft_verification.c
@@ -6,11 +6,38 @@
 /*   By: jfieux <jfieux@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 16:46:38 by jfieux            #+#    #+#             */
-/*   Updated: 2021/03/17 11:34:02 by jfieux           ###   ########.fr       */
+/*   Updated: 2021/03/17 12:21:16 by jfieux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+char	*ft_verif_nbs_c(t_struct *info, char *flag, int *len_flag, int *i)
+{
+	if (info->data[info->cnt - (*i)] == '.')
+	{
+		flag[(*len_flag)] = info->data[info->cnt - (*i)];
+		(*len_flag)--;
+		(*i)++;
+	}
+	if (info->data[info->cnt - (*i)] == '*')
+	{
+		flag[(*len_flag)] = info->data[info->cnt - (*i)];
+		(*len_flag)--;
+		(*i)++;
+	}
+	else
+	{
+		while (info->data[info->cnt - (*i)] <= '9' &&
+		info->data[info->cnt - (*i)] >= '0')
+		{
+			flag[(*len_flag)] = info->data[info->cnt - (*i)];
+			(*len_flag)--;
+			(*i)++;
+		}
+	}
+	return (flag);
+}
 
 int		ft_verif_c(char **flag_ref, int len_flag, t_struct *info)
 {
@@ -21,28 +48,7 @@ int		ft_verif_c(char **flag_ref, int len_flag, t_struct *info)
 	i = 1;
 	if (info->letter == 'c' && info->l > 2)
 		return (-1);
-	if (info->data[info->cnt - i] == '.')
-	{
-		flag[len_flag] = info->data[info->cnt - i];
-		len_flag--;
-		i++;
-	}
-	if (info->data[info->cnt - i] == '*')
-	{
-		flag[len_flag] = info->data[info->cnt - i];
-		len_flag--;
-		i++;
-	}
-	else
-	{
-		while (info->data[info->cnt - i] <= '9' &&
-		info->data[info->cnt - i] >= '0')
-		{
-			flag[len_flag] = info->data[info->cnt - i];
-			len_flag--;
-			i++;
-		}
-	}
+	flag = ft_verif_nbs_c(info, flag, &len_flag, &i);
 	while (info->data[info->cnt - i] == '-')
 	{
 		flag[len_flag] = info->data[info->cnt - i];
@@ -55,48 +61,65 @@ int		ft_verif_c(char **flag_ref, int len_flag, t_struct *info)
 	return (0);
 }
 
-int		ft_verif_id(char **flag_ref, int len_flag, t_struct *info, int i)
+int		ft_verif_nbz_id(t_struct *info, int *len_flag, char **flag_ref, int *i)
 {
-	char	*flag;
+	char *flag;
 
 	flag = *flag_ref;
 	if ((info->letter == 'i' || info->letter == 'd') && info->l > 2)
 		return (-1);
 	if (info->letter == '%' && info->l > 0)
 		return (-1);
-	if (info->data[info->cnt - i] == '*')
+	if (info->data[info->cnt - (*i)] == '*')
 	{
-		flag[len_flag--] = info->data[info->cnt - i];
-		i++;
+		flag[(*len_flag)--] = info->data[info->cnt - (*i)];
+		(*i)++;
 	}
 	else
 	{
-		while (info->data[info->cnt - i] >= '0' &&
-		info->data[info->cnt - i] <= '9' && len_flag >= 0)
+		while (info->data[info->cnt - (*i)] >= '0' &&
+		info->data[info->cnt - (*i)] <= '9' && (*len_flag) >= 0)
 		{
-			flag[len_flag--] = info->data[info->cnt - i];
-			i++;
+			flag[(*len_flag)--] = info->data[info->cnt - (*i)];
+			(*i)++;
 		}
 	}
-	if (info->data[info->cnt - i] == '.')
+	*flag_ref = flag;
+	return (0);
+}
+
+char	*ft_verif_nbs_id(t_struct *info, char *flag, int *i, int *len_flag)
+{
+	if (info->data[info->cnt - (*i)] == '.')
 	{
-		flag[len_flag--] = info->data[info->cnt - i];
-		i++;
-		if (info->data[info->cnt - i] == '*')
+		flag[(*len_flag)--] = info->data[info->cnt - (*i)];
+		(*i)++;
+		if (info->data[info->cnt - (*i)] == '*')
 		{
-			flag[len_flag--] = info->data[info->cnt - i];
-			i++;
+			flag[(*len_flag)--] = info->data[info->cnt - (*i)];
+			(*i)++;
 		}
 		else
 		{
-			while (len_flag >= 0 && info->data[info->cnt - i] >= '0' &&
-			info->data[info->cnt - i] <= '9')
+			while ((*len_flag) >= 0 && info->data[info->cnt - (*i)] >= '0' &&
+			info->data[info->cnt - (*i)] <= '9')
 			{
-				flag[len_flag--] = info->data[info->cnt - i];
-				i++;
+				flag[(*len_flag)--] = info->data[info->cnt - (*i)];
+				(*i)++;
 			}
 		}
 	}
+	return (flag);
+}
+
+int		ft_verif_id(char **flag_ref, int len_flag, t_struct *info, int i)
+{
+	char	*flag;
+
+	flag = *flag_ref;
+	if (ft_verif_nbz_id(info, &len_flag, &flag, &i) < 0)
+		return (-1);
+	flag = ft_verif_nbs_id(info, flag, &i, &len_flag);
 	while (len_flag >= 0 && (info->data[info->cnt - i] == '-' ||
 	info->data[info->cnt - i] == ' ' || info->data[info->cnt - i] == '+' ||
 	info->data[info->cnt - i] == '0' || info->data[info->cnt - i] == 'l'))
@@ -115,49 +138,66 @@ int		ft_verif_id(char **flag_ref, int len_flag, t_struct *info, int i)
 	return (0);
 }
 
-int		ft_verif_spuxx(char **flag_ref, int len_flag, t_struct *info, int i)
+int		ft_verif_nbz_spuxx(t_struct *info, int *len_flag, int *i, char **f_ref)
 {
-	char	*flag;
+	char *flag;
 
-	flag = *flag_ref;
+	flag = *f_ref;
 	if ((info->letter == 'u' || info->letter == 'x' || info->letter == 'X' ||
 	info->letter == 's') && info->l > 2)
 		return (-1);
 	if (info->letter == 'p' && info->l > 0)
 		return (-1);
-	if (info->data[info->cnt - i] == '*')
+	if (info->data[info->cnt - (*i)] == '*')
 	{
-		flag[len_flag--] = info->data[info->cnt - i];
-		i++;
+		flag[(*len_flag)--] = info->data[info->cnt - (*i)];
+		(*i)++;
 	}
 	else
 	{
-		while (len_flag >= 0 && info->data[info->cnt - i] >= '0' &&
-		info->data[info->cnt - i] <= '9')
+		while ((*len_flag) >= 0 && info->data[info->cnt - (*i)] >= '0' &&
+		info->data[info->cnt - (*i)] <= '9')
 		{
-			flag[len_flag--] = info->data[info->cnt - i];
-			i++;
+			flag[(*len_flag)--] = info->data[info->cnt - (*i)];
+			(*i)++;
 		}
 	}
-	if (info->data[info->cnt - i] == '.')
+	*f_ref = flag;
+	return (0);
+}
+
+char	*ft_verif_nbs_spuxx(t_struct *info, int *len_flag, int *i, char *flag)
+{
+	if (info->data[info->cnt - (*i)] == '.')
 	{
-		flag[len_flag--] = info->data[info->cnt - i];
-		i++;
-		if (info->data[info->cnt - i] == '*')
+		flag[(*len_flag)--] = info->data[info->cnt - (*i)];
+		(*i)++;
+		if (info->data[info->cnt - (*i)] == '*')
 		{
-			flag[len_flag--] = info->data[info->cnt - i];
-			i++;
+			flag[(*len_flag)--] = info->data[info->cnt - (*i)];
+			(*i)++;
 		}
 		else
 		{
-			while (len_flag >= 0 && info->data[info->cnt - i] >= '0' &&
-			info->data[info->cnt - i] <= '9')
+			while ((*len_flag) >= 0 && info->data[info->cnt - (*i)] >= '0' &&
+			info->data[info->cnt - (*i)] <= '9')
 			{
-				flag[len_flag--] = info->data[info->cnt - i];
-				i++;
+				flag[(*len_flag)--] = info->data[info->cnt - (*i)];
+				(*i)++;
 			}
 		}
 	}
+	return (flag);
+}
+
+int		ft_verif_spuxx(char **flag_ref, int len_flag, t_struct *info, int i)
+{
+	char	*flag;
+
+	flag = *flag_ref;
+	if (ft_verif_nbz_spuxx(info, &len_flag, &i, &flag) < 0)
+		return (-1);
+	ft_verif_nbs_spuxx(info, &len_flag, &i, flag);
 	while (len_flag >= 0 && (info->data[info->cnt - i] == '-'
 	|| info->data[info->cnt - i] == '0' || info->data[info->cnt - i] == '#' ||
 	info->data[info->cnt - i] == 'l'))
