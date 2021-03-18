859ec75bdfdf6b102068bddceae80a958bfbdddf
julesfieux <julesfieux@yahoo.fr>
2021-03-18 11:43:47 +0100

    norm

--- ft_treating.c
+++ ft_treating.c
@@ -6,7 +6,7 @@
 /*   By: jfieux <jfieux@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 10:39:00 by jfieux            #+#    #+#             */
-/*   Updated: 2021/03/17 17:16:48 by jfieux           ###   ########.fr       */
+/*   Updated: 2021/03/18 11:43:22 by jfieux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,28 +56,12 @@ int		ft_treat_str(char *str, t_struct *info)
 	return (1);
 }
 
-int		ft_treat_pointer(unsigned long long adress, t_struct *info)
+char	*ft_treat_pointer1(unsigned long long adress, int len, char *tmp)
 {
-	int					len;
 	unsigned long long	temp;
 	const char			base[16] = "0123456789abcdef";
-	char				null[3] = "0x0";
-	char				*tmp;
 
 	temp = adress;
-	len = 2;
-	if (adress == 0)
-	{
-		if (!(tmp = malloc(sizeof(char) * 6)))
-			return (0);
-		len = 0;
-		while (len < 3)
-		{
-			tmp[len] = null[len];
-			len++;
-		}
-		tmp[len] = '\0';
-	}
 	if (adress != 0)
 	{
 		while (temp > 0)
@@ -97,6 +81,29 @@ int		ft_treat_pointer(unsigned long long adress, t_struct *info)
 		}
 		tmp[len] = base[adress];
 	}
+	return (tmp);
+}
+
+int		ft_treat_pointer(unsigned long long adress, t_struct *info)
+{
+	int					len;
+	const char			null[3] = "0x0";
+	char				*tmp;
+
+	len = 2;
+	if (adress == 0)
+	{
+		if (!(tmp = malloc(sizeof(char) * 6)))
+			return (0);
+		len = 0;
+		while (len < 3)
+		{
+			tmp[len] = null[len];
+			len++;
+		}
+		tmp[len] = '\0';
+	}
+	tmp = ft_treat_pointer1(adress, len, tmp);
 	info->arg = tmp;
 	return (1);
 }
@@ -137,9 +144,9 @@ int		ft_treat_uint(unsigned long long int nb, t_struct *info)
 
 int		ft_treat_hexa(unsigned long long int nb, t_struct *info, int maj)
 {
-	int			i;
+	int						i;
 	unsigned long long int	num;
-	char		*tmp;
+	char					*tmp;
 
 	i = 0;
 	num = nb;
